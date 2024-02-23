8b434797972d30c425941e0f02c03335bb02da43
ASHISH VERMA <ashishkv845@gmail.com>
2024-02-23 16:39:25 +0530

    add

--- src/component/Canvas.jsx
+++ src/component/Canvas.jsx
@@ -3,7 +3,11 @@ import PhoneFrame from "./PhoneFrame";
 import { useImage } from "../context/Context";
 import DropArea from "./DropArea";
 import html2canvas from "html2canvas";
+import cacti from "../../public/backgrounds/cacti.jpg";
+import flowers from "../../public/backgrounds/flowers.jpg";
+import lama from "../../public/backgrounds/lama.jpg";
 
+import sunset from "../../public/backgrounds/sunset.jpg";
 function Canvas() {
   const canvasRef = useRef(null);
   const { addImage, addImagePath } = useImage();
@@ -32,12 +36,55 @@ function Canvas() {
         className="py-3 px-5 bg-fuchsia-800 rounded-md font-semibold text-white shadow-lg"
       />
 
-      <button
+      {/* <button
         onClick={handleDownloadClick}
         className="px-5 py-2  bg-red-300 h-12 w-40 "
       >
         Download
-      </button>
+      </button> */}
+      <p className="text-center text-white  py-2 font-semibold">Select Image</p>
+      <div className="border p-2 grid grid-cols-2 md:grid-cols-2 gap-3">
+        <div>
+          <img
+            src={lama}
+            alt=""
+            className="w-full"
+            onClick={(e) => {
+              addImagePath(e.target.src);
+            }}
+          />
+        </div>
+        <div>
+          <img
+            src={cacti}
+            alt=""
+            className="w-full"
+            onClick={(e) => {
+              addImagePath(e.target.src);
+            }}
+          />
+        </div>
+        <div>
+          <img
+            src={flowers}
+            alt=""
+            className="w-full"
+            onClick={(e) => {
+              addImagePath(e.target.src);
+            }}
+          />
+        </div>
+        <div>
+          <img
+            src={sunset}
+            alt=""
+            className="w-full"
+            onClick={(e) => {
+              addImagePath(e.target.src);
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
